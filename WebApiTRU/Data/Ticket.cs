9556cb58c15f6cd909d7c23b26b445ec03a0681c
BensonB12 <89985506+BensonB12@users.noreply.github.com>
2024-02-07 16:07:40 -0700

    successfully scaffolded

---
+++ WebApiTRU/Data/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApiTRU.Data;
+
+public partial class Ticket
+{
+    public int Id { get; set; }
+
+    public string Qrhash { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public int? ConcertId { get; set; }
+
+    public DateTime? Timescanned { get; set; }
+
+    public virtual Concert? Concert { get; set; }
+}
