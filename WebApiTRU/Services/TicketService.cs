8e1dd2388d7e5af8a0cc9e3ee1c701d4180dc3e6
Benson Bird <benson.bird@students.snow.edu>
2024-02-08 15:50:34 -0700

    Services made for website

---
+++ WebApiTRU/Services/TicketService.cs
@@ -0,0 +1,58 @@
+﻿using LibraryTRU.IServices;
+using Microsoft.EntityFrameworkCore;
+using WebApiTRU.Data;
+
+namespace WebApiTRU.Services
+{
+    public class TicketService : ITicketService
+    {
+        PostgresContext _ticketContext;
+        public TicketService(PostgresContext _pgContext)
+        {
+            _ticketContext = _pgContext;
+        }
+
+        public async Task<Ticket> Add(string email, int concertId)
+        {
+            Ticket ticket = MakeTicketFrom(email, concertId);
+            await _ticketContext.Tickets.AddAsync(ticket);
+            await _ticketContext.SaveChangesAsync();
+            return ticket;
+        }
+
+        public async Task<IEnumerable<Ticket>> GetAll()
+        {
+            return await _ticketContext.Tickets.ToListAsync();
+        }
+
+        public async Task ScanTicket(string qrHash)
+        {
+            Ticket target = await _ticketContext.Tickets.Where(qr => qr.Qrhash == qrHash).SingleAsync();
+            target.Timescanned = DateTime.Now;
+            _ticketContext.Update(target);
+            await _ticketContext.SaveChangesAsync();
+        }
+
+        private Ticket MakeTicketFrom(string email, int concertId)
+        {
+            Ticket ticket = new Ticket()
+            {
+                Email = email,
+                ConcertId = concertId,
+                Qrhash = GenerateTicketHash()
+            };
+
+            return ticket;
+        }
+
+        private string GenerateTicketHash()
+        {
+            string hash;
+            char[] chars = new char[16];
+
+            return chars.ToString();
+            
+        }
+    
+    }
+}
