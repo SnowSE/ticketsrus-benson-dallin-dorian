b95b266568b59be2d41098be5213b048b622e8ed
Benson Bird <benson.bird@students.snow.edu>
2024-02-08 15:27:07 -0700

    added services

---
+++ LibraryTRU/IServices/ITicketService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiTRU.Data;
+
+namespace LibraryTRU.IServices
+{
+    public interface ITicketService
+    {
+        public Task<IEnumerable<Ticket>> GetAll();
+        public Task<Ticket> Add(string email, int concertId);
+        public Task ScanTicket(string qrHash);
+    }
+}
