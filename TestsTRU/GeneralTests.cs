a3ff58e25e035ecae4bd8ed619181b29f944c746
Dorian Cottle <thenewdorian21@gmail.com>
2024-02-09 12:30:09 -0700

    tests not working but almost!

---
+++ TestsTRU/GeneralTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using LibraryTRU.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsTRU
+{
+    public class GeneralTests : IClassFixture<TRUWebAppFactory>
+    {
+        HttpClient client;
+        public GeneralTests(TRUWebAppFactory factory)
+        {
+            client = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async void GetAllTicketsTest()
+        {
+            var tickets = await client.GetFromJsonAsync<IEnumerable<Ticket>>("api/ticket/getall");
+            tickets.Where(o => o.Id == 1).Should().HaveCount(1);
+        }
+    }
+}
