140afa2f0b4943e8e876c656dc5378f92b7bddff
Benson Bird <bensonbird2@gmail.com>
2024-02-09 15:59:54 -0700

    Added Ethans new stuff, but it won't run on the computer because... IDK. But it does run on an android emulator
    Also added a MakeQRCodePage

---
+++ MauiTRU/CameraPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ZXing.Net.Maui;
+
+namespace MauiTRU;
+
+partial class CameraPage : ContentPage
+{
+    public CameraPage()
+    {
+        InitializeComponent();
+
+        barcodeReader.Options = new BarcodeReaderOptions{
+            Formats = BarcodeFormat.QrCode,
+            AutoRotate = true,
+            Multiple = true,
+            TryHarder = true,
+        };
+    }
+
+    private TaskCompletionSource<BarcodeResult> scanTask = new TaskCompletionSource<BarcodeResult>();
+    public async Task<BarcodeResult> WaitForResultAsync()
+    {
+        var value = await scanTask.Task;
+
+        scanTask = new TaskCompletionSource<BarcodeResult>();
+
+        return value;
+    }
+
+    protected void barcodeReader_BarcodesDetected(object sender, BarcodeDetectionEventArgs e)
+    {
+        scanTask.TrySetResult(e.Results.FirstOrDefault());
+    }
+}
