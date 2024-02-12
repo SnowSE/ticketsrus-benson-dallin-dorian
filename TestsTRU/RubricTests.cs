e98e1cf82c369ec75d529f56325861fb6d2297fe
Dorian Cottle <thenewdorian21@gmail.com>
2024-02-11 22:21:46 -0700

    made one tiny change

--- TestsTRU/RubricTests.cs
+++ TestsTRU/RubricTests.cs
@@ -25,15 +25,19 @@ public class RubricTests : IClassFixture<TRUWebAppFactory>
         TicketDTO data = new() {Email =  testEmail, ConcertId = 1};
         var result = await client.PostAsJsonAsync("/api/ticket/new", data);
         var ticket = result.Content.ReadFromJsonAsync<Ticket>().Result;
+        int id = ticket.Id;
+
+        //make sure the ticket hasnt been scanned
+        var testResult = await client.GetFromJsonAsync<Ticket>($"api/ticket/{id}");
+        testResult.Timescanned.Should().BeNull();
 
         //Act: Scan the ticket
         var qrHash = ticket.Qrhash;
         await client.PutAsJsonAsync("/api/ticket/scan", qrHash);
 
         //Assert: get the ticket and see if it was scanned
-        int id = ticket.Id;
-        var testResult = await client.GetFromJsonAsync<Ticket>($"api/ticket/{id}");
-        testResult.Timescanned.Should().NotBeNull();
+        var testResult2 = await client.GetFromJsonAsync<Ticket>($"api/ticket/{id}");
+        testResult2.Timescanned.Should().NotBeNull();
      
     }
 
