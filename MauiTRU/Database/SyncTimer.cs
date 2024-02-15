706124c389cfe11746cd8786a26e68a26e0ab30e
DJPhelps1 <46822189+DJPhelps1@users.noreply.github.com>
2024-02-15 09:33:34 -0700

    Database Sync class made

---
+++ MauiTRU/Database/SyncTimer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTRU.Database
+{
+    public sealed class SyncTimer
+    {
+        private TimeSpan timer = TimeSpan.FromSeconds(30);
+        private LocalTRUDatabase? database;
+        private bool cancellationflag = true;
+        public TimeSpan TimerSteward {
+            get; set;
+        }
+
+        public void SetDatabase(LocalTRUDatabase db)
+        {
+            database = db;
+        }
+
+        public async Task StartThread()
+        {
+            await SyncThread();
+        }
+
+        public void StopThread()
+        {
+            cancellationflag = false;
+        }
+
+        public async Task SyncThread()
+        {
+            while (cancellationflag)
+            {
+                Thread.Sleep(timer);
+                if(database is not null)
+                { 
+                    await database.UpdateLocalDbFromMainDb();
+                    await database.UpdateMainDbFromLocalDb();
+                }
+            }
+        }
+    }
+}
