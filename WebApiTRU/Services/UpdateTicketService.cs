67befbc5ab6f38c6d6fffa49342eac4fa5b5c40a
Dorian Cottle <thenewdorian21@gmail.com>
2024-02-17 23:12:34 -0700

    api updates when api db changes

---
+++ WebApiTRU/Services/UpdateTicketService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiTRU.Services
+{
+    public class UpdateTicketService
+    {
+        PostgresContext _ticketContext;
+        public UpdateTicketService(IDbContextFactory<PostgresContext> contxtFact)
+        {
+            _ticketContext = contxtFact.CreateDbContext();
+        }
+
+
+    }
+}
