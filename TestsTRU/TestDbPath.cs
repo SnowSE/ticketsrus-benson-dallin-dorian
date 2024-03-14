da590f9190ee6d865bb4c0f051e4b63695a01cc5
Benson Bird <benson.bird@students.snow.edu>
2024-03-13 18:28:34 -0600

    GOOODBYEE MAUII

--- TestsTRU/TestDbPath.cs
+++ TestsTRU/TestDbPath.cs
@@ -1,14 +1,1 @@
-﻿using MauiTRU.Database;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestsTRU
-{
-    public class TestDbPath : IDbPath
-    {
-        public string Directory => System.IO.Directory.GetCurrentDirectory();
-    }
-}
+﻿
