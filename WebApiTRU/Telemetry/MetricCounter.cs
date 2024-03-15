18e785b596376c28d80495aa4f2e734ae621a03b
BensonB12 <89985506+BensonB12@users.noreply.github.com>
2024-03-14 22:15:32 -0600

    metric?

---
+++ WebApiTRU/Telemetry/MetricCounter.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace WebApiTRU.Telemetry
+{
+    public static class MetricCounter
+    {
+        public const string MetricCounterName = "Benson.Telemetry.Homwork.Counter";
+        public static Meter MetricCounterMeter = new(MetricCounterName, "1.0.0");
+    }
+}
